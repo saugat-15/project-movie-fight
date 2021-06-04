97f56ea9db8751caa0a6b9d245fe1e9df2004ef3
Saugat Giri <Saugatgiri15@gmail.com>
2021-06-04 20:25:37 +1000

    SSL certificate updated

--- index.js
+++ index.js
@@ -11,7 +11,7 @@ const autocompleteConfig = {
         return movie.Title;
     },
     async fetchData(searchTerm) {
-        const response = await axios.get('http://www.omdbapi.com/', {
+        const response = await axios.get('https://www.omdbapi.com/', {
             params: {
                 apikey: '66b4ee3a',
                 s: searchTerm,
